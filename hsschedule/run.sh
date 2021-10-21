e33b9c4a990c5e63ac92f4ef0ec174818d709f59
Gavin <gavin@hisharp.com.tw>
2021-10-21 09:20:01 +0000

    add hs

--- hsschedule/run.sh
+++
@@ -1,3 +0,0 @@
-touch /tmp/hs_schedule_hscron_06.log;
-sleep 1;
-sudo docker-compose up -d;
--- hsweb/run.sh
+++
@@ -1,7 +0,0 @@
-touch /tmp/supervisord_webfms_06_80.log;
-touch /tmp/kafka_service_webfms_06_80.log;
-touch /tmp/access_webfms_06_80.log;
-touch /tmp/error_webfms_06_80.log;
-touch /tmp/uwsgi_0.5_webfms_06_80.log
-sleep 1;
-sudo docker-compose up -d;
